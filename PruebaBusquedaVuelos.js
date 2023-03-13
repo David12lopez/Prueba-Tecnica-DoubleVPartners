eb8bb3c3566cc279515a08a2b53dc7cda10dc7d0
David <david12lopez10@gmail.com>
2023-03-13 16:52:59 -0500

    Actualizar README.md

--- PruebaBusquedaVuelos.js
+++ PruebaBusquedaVuelos.js
@@ -16,6 +16,7 @@ describe('Vuelos Bogotá-Cali', () => {
       cy.get('.icon-chev:nth-child(4)').click();
       cy.get('#inputOutboundDate .next:nth-child(1)').click();
       cy.get('#inputOutboundDate .month1:nth-child(1) tr:nth-child(4) > td:nth-child(4) > .day:nth-child(1)').click();
-      cy.get('#date-window-container .month2 tr:nth-child(4) > td:nth-child(4) > .day').click();   
+      cy.get('#date-window-container .month2 tr:nth-child(4) > td:nth-child(4) > .day').click();
+      cy.get('.btn-search').click({force: true});
     })
   })
